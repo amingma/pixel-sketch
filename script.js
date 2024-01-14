80e14c858d6f4527f8ebc75ab76044fd72bfb96c
amingma <jiama19999@gmail.com>
2024-01-14 17:21:58 -0600

    Works, but needs responsive changes

--- script.js
+++ script.js
@@ -5,28 +5,12 @@ const pageContainer = document.querySelector('#container');
 const penToggler = document.querySelector('#pen-toggle');
 const rainbowToggler = document.querySelector('#rainbow-toggle');
 const inputButton = document.querySelector('#pixel');
+const eraseButton = document.getElementById('erase');
 
 drawGrid(16);
 
 penToggler.addEventListener('click', ()=>{
-    penOn = !penOn;
-    penToggler.replaceChildren();
-    const one = document.createElement('span');
-    one.textContent = "Pen: ";
-    const two = document.createElement('span');
-    two.textContent = "ON";
-    const three = document.createElement('span');
-    three.textContent = '/';
-    const four = document.createElement('span');
-    four.textContent = "OFF";
-    if (penOn) {
-        two.className = "strong";
-    }
-    else {
-        four.className = "strong";
-    }
-    penToggler.appendChild(one); penToggler.appendChild(two);
-    penToggler.appendChild(three); penToggler.appendChild(four);
+    togglePen();
 });
 
 rainbowToggler.addEventListener('click', ()=>{
@@ -58,6 +42,16 @@ function drawGrid(sideLength) {
             const gridDiv = document.createElement('div');
             gridDiv.className = "flex-item"
             flexContainer.appendChild(gridDiv);
+            gridDiv.addEventListener('mouseover', ()=>{
+                if (penOn) {
+                    if (isRainbow) {
+                        gridDiv.style.backgroundColor = generateRandomColor();
+                    }
+                    else {
+                        gridDiv.style.backgroundColor = "black";
+                    }
+                }
+            });
         }
         pageContainer.appendChild(flexContainer);
     }    
@@ -66,6 +60,18 @@ function drawGrid(sideLength) {
 function removeGrid() {
     pageContainer.replaceChildren();
 }
+
+function generateRandomColor() {
+    const arr = [];
+    for (let i=0; i<3; i++) {
+        arr.push(Math.floor(Math.random()*256));
+    }
+    return `rgb(${arr[0]}, ${arr[1]}, ${arr[2]})`;
+}
+
+pageContainer.addEventListener('click', ()=>{
+    togglePen();
+});
 
 inputButton.addEventListener('change', ()=>{
     let sideLength = inputButton.value;
@@ -77,3 +83,31 @@ inputButton.addEventListener('change', ()=>{
         alert("Invalid input");
     }
 });
+
+eraseButton.addEventListener('click', ()=>{
+    const boxes = document.querySelectorAll('.flex-item');
+    boxes.forEach(box=>{
+        box.style.backgroundColor = 'white';
+    });
+});
+
+function togglePen() {
+    penOn = !penOn;
+    penToggler.replaceChildren();
+    const one = document.createElement('span');
+    one.textContent = "Pen: ";
+    const two = document.createElement('span');
+    two.textContent = "ON";
+    const three = document.createElement('span');
+    three.textContent = '/';
+    const four = document.createElement('span');
+    four.textContent = "OFF";
+    if (penOn) {
+        two.className = "strong";
+    }
+    else {
+        four.className = "strong";
+    }
+    penToggler.appendChild(one); penToggler.appendChild(two);
+    penToggler.appendChild(three); penToggler.appendChild(four);
+}
